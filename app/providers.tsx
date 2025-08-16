16ed2417c3aebd063b578548065b954736ffdd41
swarup-kumar-sahoo <iamswarupkumar7@gmail.com>
2025-08-16 12:21:53 +0530

    Update for production

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,9 @@
 "use client";
 import { ClerkProvider } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
-  // For Clerk redirect handling in App Router
-  const returnUrl =
-    typeof window !== "undefined" ? window.location.origin + pathname : "";
-
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      options={{ redirectUrl: returnUrl }}
-    >
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       {children}
     </ClerkProvider>
   );
